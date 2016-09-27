77760d0c0d0e2f4954faa87d0fa3019516c85d3d
ramosjanoah <ramosj.noah@gmail.com>
2016-09-27 18:30:51 +0700

    Udah bisa ganti jadwal manual
    All changes in modifyJadwal.php

--- modifyJadwal.php
+++ modifyJadwal.php
@@ -4,15 +4,39 @@ session_start();
 $arrayRuangan = $_SESSION["arrayRuangan"];
 $indexRuangan = $_SESSION["indexRuangan"];
 $indexMatkul = $_SESSION["indexMatkul"];
-$lala = $_POST["pindahKe"];
-echo $lala;
+
+
+$idxMatkul = $_POST["changeMatkul"];
+$harijam = $_POST["pindahKe"];
+
+//echo $idxMatkul;
+
+$count = 0;
+
 
 $arrayRuangan[0][0][1] = 0;
 //ALGO LU
 
+foreach ($arrayRuangan as $idxRuangan => $ruangan) {
+	foreach ($ruangan[$idxMatkul] as $idxJam => $jam) {
+			if ($jam[1]) {
+				$arrayRuangan[$idxRuangan]
+				[$idxMatkul]
+				[$idxJam]
+				[1] = false;
+				$count++;
+			}
+		}
+	}
+
+echo $count;
+
+
 $_SESSION["arrayRuangan"] = $arrayRuangan;
 $_SESSION["arrayRuangan"] = $arrayRuangan;
 $_SESSION["arrayRuangan"] = $arrayRuangan;
 
-header("Location: /aischeduling/result.php");
+// header("Location: /GitHub/Tubes1AI/result.php");
+
+
 ?>
