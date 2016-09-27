c68c5945c6a6706b146277d3ebcd055093466208
alsoncahyadi <si_cool_alson@yahoo.com>
2016-09-28 00:22:04 +0700

    lagi lagi minor

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ $(document).ready(function(){
 	//kalo class data diklik...
     $(".data").click(function(event){
     	var matkulId = $(event.target).attr('class').replace('data ','');
+    	matkulId = matkulId.replace(' selecteddata','');
     	var initialColorName = "rgba(0, 0, 0, 0)";
     	var changeColorName = "rgb(0, 255, 0)";
     	//Semua class matkulId diubah warnanya jadi ijo
 		$("." + matkulId).each(function() {
-			if ($(this).css("background-color") != changeColorName) {
+			if ($(this).attr("class").indexOf("selecteddata") == -1) {
 				$('input[name=changeMatkul]').val(matkulId);
-				$(this).css({"background-color": changeColorName, "border" : "1px solid black", "font-weight" : "bolder"});
+				$(this).addClass('selecteddata');
 			} else { //Kalo udah ijo, diputihin
 				$('input[name=changeMatkul]').val("");
-				$(this).css({"background-color": initialColorName, "border" : "0px", "font-weight" : "normal"});
+				$(this).removeClass('selecteddata');
 			}
 		});
     });
@@ -25,12 +26,12 @@ $(document).ready(function(){
 		var hariJam = event.target.id;
 		//cell dengan id hariJam dikuningin
 		if ($(event.target).attr("class").indexOf("data") == -1) {
-			if ($(this).css("background-color") != changeColorName) {
+			if ($(this).attr("class").indexOf("selectedharijam") == -1) {
 				$('input[name=pindahKe]').val(hariJam);
-				$(this).css({"background-color": changeColorName});
+				$(this).addClass("selectedharijam");
 			} else {//kalo udah kuning, diputihin
 				$('input[name=pindahKe]').val("");
-				$(this).css({"background-color": initialColorName});
+				$(this).removeClass("selectedharijam");
 			}
 		} else {
 		}
